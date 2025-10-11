7c005d2ffa88068fc0c2ea153841d926409c2832
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-10-11 21:46:46 +0530

    Project Setup with react + tsx!

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-base-200">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-primary text-2xl">Hello DaisyUI 🌸</h2>
+          <p className="text-lg">React + TypeScript + Tailwind + daisyUI</p>
+          <div className="card-actions mt-4">
+            <button className="btn btn-primary">Click Me</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
