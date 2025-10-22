e0864210f5621138ee45ddc4889220f95c3cf596
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-10-22 14:16:23 +0530

    Layout Created for window size!

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import words from './wordList.json';
-
+import Navbar from "./components/Navbar"
 function App() {
   const [wordToGuess, setWordToGuess] = useState(()=>
   {
     return words[Math.floor(Math.random()*words.length)]
   });
   return (
-    <div className="min-h-screen flex justify-center bg-base-300">
+    <div className="min-h-screen flex flex-col bg-base-300">
+      <Navbar/>
       <div>{wordToGuess}</div>
       <div className="card w-96 bg-base-100 shadow-xl mt-20">
         <div className="card-body items-center text-center">
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gradient-to-l from-black via-[#160c0c] to-[#121727] flex-col flex justify-center items-center h-14 w-full '>
+      <div className='border'>Navbar</div>
+    </div>
+  )
+}
+
+export default Navbar
