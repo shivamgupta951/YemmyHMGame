e5f09dd0fb9a672bb9f7293159f8beb35da13e40
Shivam Gupta <kapilmohangupta20@gmail.com>
2025-11-07 21:52:35 +0530

    Game Section layout created!

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,12 +3,18 @@ import Video from "./video";
 import image from "../assets/image.png";
 import { motion } from "framer-motion";
 import { FaGamepad } from "react-icons/fa";
+import { TbDeviceGamepad3Filled } from "react-icons/tb";
+import { BiCookie } from "react-icons/bi";
+import GameSection from "./GameSection";
 
 const Home = () => {
   return (
-    <div className="border-b-2 border-dashed border-red-400 pb-32 w-full min-h-[900px] lg:min-h-[850px]">
+    <div className="pb-10 w-full min-h-[1800px] lg:min-h-[1550px]">
       <div className="flex flex-col w-full lg:flex-row justify-around items-center">
-        <div className="hidden lg:flex rounded-md shadow-lg shadow-orange-400 from-black via-black to-purple-950 w-[50%] mt-14 py-20 bg-gradient-to-br items-center justify-center">
+        <div className="hidden lg:flex rounded-md shadow-lg shadow-orange-400 from-black via-black to-[#320521] w-[50%] mt-14 py-10 bg-gradient-to-br items-center justify-center flex-col pb-16">
+          <div className="my-6 text-lg font-bold text-purple-800">
+            Play For Win , Win To Play!
+          </div>
           <Video />
         </div>
         <div className="flex justify-center items-center flex-col space-y-12 relative w-[70%] lg:w-[30%]">
@@ -32,18 +38,32 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="flex shadow-lg rounded-md shadow-orange-400 from-black via-black to-purple-950 w-[80%] mt-14 p-12 py-16 bg-gradient-to-br items-center justify-center lg:hidden">
+        <div className="flex shadow-lg rounded-md shadow-orange-400 from-black via-black to-[#320521] w-[80%] mt-14 p-12 py-20 pt-14 bg-gradient-to-br items-center justify-center flex-col lg:hidden">
+          <div className="my-6 text-lg font-bold text-purple-800">
+            Play For Win , Win To Play!
+          </div>
+
           <Video />
         </div>
+      </div>
+      <div className="mt-16 flex justify-center items-center text-lg text-transparent bg-gradient-to-r bg-clip-text from-black via-orange-900 to-red-900">
+        <TbDeviceGamepad3Filled className="animate-slow-spin text-red-800 mx-1 text-xl"></TbDeviceGamepad3Filled>
+        Scroll Down For Game Section!
       </div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 1.5 }}
-        className="text-center mt-40 text-gray-400 text-xl flex flex-col justify-center items-center"
+        transition={{
+          delay: 1.5,
+          repeat: Infinity,
+          repeatDelay: 2,
+          duration: 4,
+        }}
+        className="text-center mt-32 text-gray-400 text-xl flex flex-col justify-center items-center pb-48 border-b-4 border-dashed border-red-400"
       >
-        <h2 className="text-2xl flex justify-center items-center my-7 text-yellow-700 font-bold tracking-wider underline shadow-xl shadow-black w-[40%]">
-          YEMMYHMGAME <FaGamepad className="mx-3"></FaGamepad>
+        <h2 className="text-2xl flex justify-center items-center my-7 text-yellow-700 font-bold tracking-wider underline shadow-xl shadow-black w-[80%]">
+          <BiCookie className="mx-2 text-primary"></BiCookie> YemmyHMGame{" "}
+          <FaGamepad className="mx-3 animate-slow-spin"></FaGamepad>
         </h2>
         <p className="text-xl w-[50%] bg-clip-text bg-gradient-to-r from-purple-800 via-orange-700 to-red-900 text-transparent">
           Test your vocabulary, sharpen your thinking, and have fun at the same
@@ -60,8 +80,8 @@ const Home = () => {
           © {new Date().getFullYear()} YemmyHMGame. All rights reserved.
         </p>
       </motion.div>
+      <div className="min-h-[700px] flex justify-center items-center flex-col"><GameSection/></div>
     </div>
   );
 };
-
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slow-spin": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        "slow-spin": "slow-spin 4s linear infinite", 
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "cupcake"],
   },
-}
+};
